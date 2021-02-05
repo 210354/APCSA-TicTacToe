2595b53072dc04c0c52babf786190380c7e80540
210354 <78184973+210354@users.noreply.github.com>
2021-02-05 23:25:43 +0800

    Finished classes

--- GameBoard.java
+++ GameBoard.java
@@ -1,123 +1,167 @@
 import java.util.*;
+import jdk.javadoc.internal.tool.resources.javadoc;
 import java.lang.*; 
 
-class GameBoard {
 
-    private static char currentPlayer = 'x'; 
-    public final static char[][] board = new char[3][3];
+class MainTicTacToe {
 
- 
-    public static void setBoard() {
-
-        System.out.println("Here is the empty game board: \n");
-        System.out.println("-------------"); 
-
-		for(int i = 0 ; i < board.length;i++)
-		{
-			for(int j = 0 ; j < board[0].length;j++)
-			{
-                board[i][j] = '-'; 
-
-                if (j % 3 == 0 ) {
-                    System.out.print("| ");
-                }
-                System.out.print(board[i][j] + " | "); 
-            }
-            
-            System.out.println();
-            System.out.println("-------------"); 
-        }
-    }
-
-    public static void printBoard() {
-        System.out.println("-------------"); 
-
-        for (int i =0; i <3; i++) {
-            System.out.print("| ");
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " | ");
-            }
-            System.out.println();
-            System.out.println("-------------"); 
-        }
-    }
+    static Scanner input = new Scanner(System.in);
+    static int row; //Need these to set position of a player's mark 
+    static int column;
     
-    
-    public static void playerChange() {
-        if (currentPlayer == 'x') {
-            currentPlayer = 'o';
+    static GameBoard game = new GameBoard();
+    public static void main(String[] args) {
+
+        dashedLines(); //Makes it neater to read 
+
+        System.out.println("\nWelcome to Tic Tac Toe!"); 
+        dashedLines(); 
+
+        game.setBoard(); //set board first 
+        dashedLines();
+
+        System.out.println("Would you like to play the (C)omputer version or 2 (P)layer version? "); //Two versions of the game
+        String version = input.nextLine(); 
+
+        if (version.equals("P")) { //To let two users play the game
+
+            runGameTwoPlayers();
+
+            while (game.checkFinalWin() == false && game.boardFull() == false) { //Keep playing moves until a tie or a win 
+                
+                GameBoard.playerChange(); //Change player after every move 
+                runGameTwoPlayers(); 
         }
-        else {
-            currentPlayer = 'x';
-        }
-    }
-
-    public static char getCurrentPlayer()
-    {
-        return currentPlayer;
-    }
-
-
-    public static boolean setPosition(int row, int col) { //sets the board
-
-        if ((row >= 0) && (row < 3)) {
-            if ((col >= 0) && (col < 3)) {
-                if (board[row][col] == '-') {
-                    board[row][col] = currentPlayer;
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    public boolean checkWinnerRowColumns(char a, char b, char c) {
-        return ((a != '-') && (a == b) && (b == c));
     } 
 
+//Computer vs player version of the game
+    else if (version.equals("C")){
 
-    public boolean checkWinnerRows() { 
+        runGameComputer(); 
 
-        for (int i = 0; i < 3; i++) {
-            if (checkWinnerRowColumns(this.board[i][0], this.board[i][1],this.board[i][2]) == true) {
-                return true;
+            while (game.checkFinalWin() == false && game.boardFull() == false) {
+                
+            GameBoard.playerChange();
+            runGameComputer();
             }
-        }
-        return false; 
+
+
     }
 
-    public boolean checkWinnerColumms(){ 
+//If the board is full with no wins, it's a tie
+    if (game.boardFull() == true ) {
+        System.out.println("It's a tie!");
 
-        for (int i = 0; i < 3; i++) {
-            if (checkWinnerRowColumns(this.board[0][i],this.board[1][i],this.board[2][i]) == true) {
-                return true;
+    }
+
+//If either player wins, then this will show (use the getCurrentPlayer() method for this)
+    else {
+        System.out.println("\nPlayer " + Character.toUpperCase(game.getCurrentPlayer()) + " wins!\n"); 
+    }
+
+
+} 
+
+
+//Asks the user for which row and column they would like to make a mark in 
+    public static void runGameTwoPlayers() {
+        System.out.println("\nPlayer " + GameBoard.getCurrentPlayer() + ", please enter the column you'd like (from 1-3): ");
+        column = input.nextInt() - 1; 
+
+        System.out.println("Now please enter the row you'd like (from 1-3): "); 
+        row = input.nextInt() - 1;
+
+        game.setPosition(row, column); //Marks position on the board
+
+        GameBoard.printBoard(); //Shows the player what the board looks like 
+        dashedLines();
+
+    }
+
+//Computer vs player version of the game
+    public static void runGameComputer() {
+
+            Random random = new Random();
+ 
+//When it's the user's turn 
+            if (GameBoard.getCurrentPlayer() == 'x') {
+
+                System.out.println("\nPlayer x, please enter the column you'd like (from 1-3): ");
+                column = input.nextInt() - 1; 
+        
+                System.out.println("Now please enter the row you'd like (from 1-3): "); 
+                row = input.nextInt() - 1;
             }
-        }
-        return false; 
-    }
-
-    public boolean checkWinnerDiagonals(){ 
-        return ((checkWinnerRowColumns(this.board[0][0], this.board[1][1], this.board[2][2]) == true || checkWinnerRowColumns(this.board[0][2],this.board[1][1],this.board[2][0]) == true));
-
-    }
-
-    public boolean checkFinalWin() {
-        return (checkWinnerRows() || checkWinnerColumms() || checkWinnerDiagonals());
-    }
 
 
-    public boolean boardFull() {
-        boolean s = true;
+//when it's the computer's turn 
+        else {
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[i][j] == '-') {
-                    s = false;
+//First priority is to get the entry in the middle of the board 
+            if (GameBoard.getPosition(1,1) == true) {
+                row = 1;
+                column = 1; 
+            }
+
+//If that's taken, it randomly generates ints to place a mark on the board 
+            else { 
+
+            int randCol = random.nextInt(2);
+            int randRow = random.nextInt(2); 
+
+//If the entry isn't taken, it makes a mark
+            if (GameBoard.getPosition(randRow, randCol) == true) {
+
+                column = randCol; 
+                row = randRow; 
+            }
+
+
+            else if (GameBoard.getPosition(randRow,randCol) == false) {
+                
+//If the entry has been marked, it runs a for loop to find an empty entry 
+/*Couldn't keep using a random number generator for this because if there's a limited number of 
+slots available, random number generator would keep going for a long time trying to look for the available slot */
+                for (int i =0; i < 3; i++) {
+                    for (int j =0; j < 3; j++) {
+
+                        if (GameBoard.getPosition(i,j) == true) {
+                            column = i;
+                            row = j; 
+                        }
+                        else {
+                            i++;
+                            j++; 
+                        }
+                    }
                 }
             }
+            }
+
+            System.out.println("The computer has played a move!");
+
         }
-        return s; 
-    }
+
+        game.setPosition(row, column); //Marks the entry of the board 
+
+        GameBoard.printBoard(); 
+        dashedLines();
 
     }
+
+
+
+    //Method to print out dashed lines to make it easier for the user to read 
+public static void dashedLines() {
+
+    System.out.println(); 
+    for (int i = 1; i <=150; i++) {
+        System.out.print("-");
+    }
+    System.out.println();
+
+}
+
+
+}
 
--- MainTicTacToe.java
+++ MainTicTacToe.java
@@ -1,43 +1,60 @@
 import java.util.*;
+import jdk.javadoc.internal.tool.resources.javadoc;
 import java.lang.*; 
-
 
 
 class MainTicTacToe {
 
     static Scanner input = new Scanner(System.in);
-    static int row;
+    static int row; //Need these to set position of a player's mark 
     static int column;
     
     static GameBoard game = new GameBoard();
     public static void main(String[] args) {
 
-
-        dashedLines(); 
+        dashedLines(); //Makes it neater to read 
 
         System.out.println("\nWelcome to Tic Tac Toe!"); 
         dashedLines(); 
-        
 
-        game.setBoard(); 
-
+        game.setBoard(); //set board first 
         dashedLines();
 
-        runGame();
+        System.out.println("Would you like to play the (C)omputer version or 2 (P)layer version? "); //Two versions of the game
+        String version = input.nextLine(); 
 
+        if (version.equals("P")) { //To let two users play the game
 
-        while (game.checkFinalWin() == false && game.boardFull() == false) {
-            
-            GameBoard.playerChange();
-            runGame(); 
+            runGameTwoPlayers();
 
+            while (game.checkFinalWin() == false && game.boardFull() == false) { //Keep playing moves until a tie or a win 
+                
+                GameBoard.playerChange(); //Change player after every move 
+                runGameTwoPlayers(); 
+        }
     } 
 
+//Computer vs player version of the game
+    else if (version.equals("C")){
+
+        runGameComputer(); 
+
+            while (game.checkFinalWin() == false && game.boardFull() == false) {
+                
+            GameBoard.playerChange();
+            runGameComputer();
+            }
+
+
+    }
+
+//If the board is full with no wins, it's a tie
     if (game.boardFull() == true ) {
         System.out.println("It's a tie!");
 
     }
 
+//If either player wins, then this will show (use the getCurrentPlayer() method for this)
     else {
         System.out.println("\nPlayer " + Character.toUpperCase(game.getCurrentPlayer()) + " wins!\n"); 
     }
@@ -45,14 +62,87 @@ class MainTicTacToe {
 
 } 
 
-    public static void runGame() {
+
+//Asks the user for which row and column they would like to make a mark in 
+    public static void runGameTwoPlayers() {
         System.out.println("\nPlayer " + GameBoard.getCurrentPlayer() + ", please enter the column you'd like (from 1-3): ");
         column = input.nextInt() - 1; 
 
         System.out.println("Now please enter the row you'd like (from 1-3): "); 
         row = input.nextInt() - 1;
 
-        game.setPosition(row, column);
+        game.setPosition(row, column); //Marks position on the board
+
+        GameBoard.printBoard(); //Shows the player what the board looks like 
+        dashedLines();
+
+    }
+
+//Computer vs player version of the game
+    public static void runGameComputer() {
+
+            Random random = new Random();
+ 
+//When it's the user's turn 
+            if (GameBoard.getCurrentPlayer() == 'x') {
+
+                System.out.println("\nPlayer x, please enter the column you'd like (from 1-3): ");
+                column = input.nextInt() - 1; 
+        
+                System.out.println("Now please enter the row you'd like (from 1-3): "); 
+                row = input.nextInt() - 1;
+            }
+
+
+//when it's the computer's turn 
+        else {
+
+//First priority is to get the entry in the middle of the board 
+            if (GameBoard.getPosition(1,1) == true) {
+                row = 1;
+                column = 1; 
+            }
+
+//If that's taken, it randomly generates ints to place a mark on the board 
+            else { 
+
+            int randCol = random.nextInt(2);
+            int randRow = random.nextInt(2); 
+
+//If the entry isn't taken, it makes a mark
+            if (GameBoard.getPosition(randRow, randCol) == true) {
+
+                column = randCol; 
+                row = randRow; 
+            }
+
+
+            else if (GameBoard.getPosition(randRow,randCol) == false) {
+                
+//If the entry has been marked, it runs a for loop to find an empty entry 
+/*Couldn't keep using a random number generator for this because if there's a limited number of 
+slots available, random number generator would keep going for a long time trying to look for the available slot */
+                for (int i =0; i < 3; i++) {
+                    for (int j =0; j < 3; j++) {
+
+                        if (GameBoard.getPosition(i,j) == true) {
+                            column = i;
+                            row = j; 
+                        }
+                        else {
+                            i++;
+                            j++; 
+                        }
+                    }
+                }
+            }
+            }
+
+            System.out.println("The computer has played a move!");
+
+        }
+
+        game.setPosition(row, column); //Marks the entry of the board 
 
         GameBoard.printBoard(); 
         dashedLines();
@@ -60,7 +150,8 @@ class MainTicTacToe {
     }
 
 
-    //Method to print out dashed lines to separate 
+
+    //Method to print out dashed lines to make it easier for the user to read 
 public static void dashedLines() {
 
     System.out.println(); 
